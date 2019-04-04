fa24005720713a7bb26dd8b52ada35a8b2556e9c
Prateek76 <iec2017029@iiita.ac.in>
2019-04-05 01:08:13 +0530

    apollo setup frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import {ApolloProver, ApolloProvider} from 'react-apollo';
+
+//components
+import BookList from './components/booklist';
+
+//apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Books list</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
