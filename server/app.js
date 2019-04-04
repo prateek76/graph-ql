7ee7211d46f7643545f764a38ff50dc4ee54dba6
Prateek76 <iec2017029@iiita.ac.in>
2019-04-05 03:52:43 +0530

    db connection error fixed

--- server/app.js
+++ server/app.js
@@ -3,10 +3,16 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
-mongoose.connect('mongodb://prateek76:prateek76@ds157667.mlab.com:57667/graphql-learn', { useNewUrlParser: true })
+//allow cross-origin express
+app.use(cors());
+
+mongoose.connect('mongodb://prateek76:prateek76@ds157667.mlab.com:57667/graphql-learn', { useNewUrlParser: true },{
+	reconnectTries: 86400	//this will rety connection all day long
+  })
 mongoose.connection.once('open',() => {
 	console.log("connected to database");
 })
