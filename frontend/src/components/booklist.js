ec44eb5318c3b179efa2900735a07b41b044d157
Prateek76 <iec2017029@iiita.ac.in>
2019-04-05 03:54:09 +0530

    queries added and compose used for multiple queries

--- frontend/src/components/booklist.js
+++ frontend/src/components/booklist.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react';
-import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
-
-const getBooksQuery = gql`
-    {
-        books {
-            name
-            id
-        }
-    }
-`
+import {getBooksQuery} from '../queries/queries';
 
 class BookList extends Component {
 
-    displayBooks() {
+    displayBooks = () => {
         var data = this.props.data;
         if(data.loading) {
             return(<div>Loading Books ... </div>);
---
+++ frontend/src/queries/queries.js
@@ -0,0 +1,30 @@
+import { gql } from 'apollo-boost';
+
+const getBooksQuery = gql`
+    {
+        books {
+            name
+            id
+        }
+    }
+`
+
+const getAuthorsQuery = gql`
+    {
+        authors {
+            name
+            id
+        }
+    }
+`
+
+const addBookMutation = gql`
+    mutation {
+        addBook(name:"",genre:"",authorId:""){
+            name
+            id
+        }
+    }
+`
+
+export { getAuthorsQuery, getBooksQuery, addBookMutation }; 
